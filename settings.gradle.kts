be23a873b6b1d9de60b7008417410c4e4ab34a93
Jared Hall <jared@apialerts.com>
2025-01-12 16:41:31 +1100

    java 17 support (was 21)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,3 +18,10 @@ include(
     ":library",
     ":sample"
 )
+
+val javaVersion = System.getProperty("java.version")?.split(".")?.firstOrNull()?.toInt() ?: Int.MAX_VALUE
+
+val requiredJava = JavaVersion.VERSION_17.toString().toInt()
+check (javaVersion >= requiredJava) {
+    "This project needs to be run with Java $requiredJava or higher (found: $javaVersion)."
+}
